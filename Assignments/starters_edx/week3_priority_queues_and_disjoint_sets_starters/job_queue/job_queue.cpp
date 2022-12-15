74f061e4f44b8e39f1c9f045323cbafc8c3a9a69
csk1116 <u1116eric777@gmail.com>
2022-12-15 17:42:36 +0800

    finish 3-2.

--- Assignments/starters_edx/week3_priority_queues_and_disjoint_sets_starters/job_queue/job_queue.cpp
+++ Assignments/starters_edx/week3_priority_queues_and_disjoint_sets_starters/job_queue/job_queue.cpp
@@ -2,34 +2,40 @@
 #include <vector>
 #include <algorithm>
 
-using std::vector;
 using std::cin;
 using std::cout;
+using std::vector;
+using std::swap;
 
-class JobQueue {
- private:
+class JobQueue
+{
+private:
   int num_workers_;
   vector<int> jobs_;
 
   vector<int> assigned_workers_;
   vector<long long> start_times_;
 
-  void WriteResponse() const {
-    for (int i = 0; i < jobs_.size(); ++i) {
+  void WriteResponse() const
+  {
+    for (int i = 0; i < jobs_.size(); ++i)
+    {
       cout << assigned_workers_[i] << " " << start_times_[i] << "\n";
     }
   }
 
-  void ReadData() {
+  void ReadData()
+  {
     int m;
     cin >> num_workers_ >> m;
     jobs_.resize(m);
-    for(int i = 0; i < m; ++i)
+    for (int i = 0; i < m; ++i)
       cin >> jobs_[i];
   }
 
-  void AssignJobs() {
-    // TODO: replace this code with a faster algorithm.
+  void AssignJobs()
+  {
+    /* TODO: replace this code with a faster algorithm.
     assigned_workers_.resize(jobs_.size());
     start_times_.resize(jobs_.size());
     vector<long long> next_free_time(num_workers_, 0);
@@ -44,17 +50,70 @@ class JobQueue {
       start_times_[i] = next_free_time[next_worker];
       next_free_time[next_worker] += duration;
     }
+    */
+    assigned_workers_.resize(jobs_.size());
+    start_times_.resize(jobs_.size());
+    vector<int> trackWorkers;
+    vector<long long> taskMinHeap(num_workers_, 0);
+    for (int i = 0; i < num_workers_; i++)
+    {
+      trackWorkers.push_back(i);
+    }
+
+    for(int i = 0; i < jobs_.size(); i++)
+    {
+      int duration = jobs_[i];
+      assigned_workers_[i] = trackWorkers[0];
+      start_times_[i] = taskMinHeap[0];
+      taskMinHeap[0] += duration;
+      siftDown(taskMinHeap, 0, trackWorkers);     
+    }
+
   }
 
- public:
-  void Solve() {
+  void siftDown(vector<long long> &data, int index, vector<int>& track)
+  {
+    int size = data.size() - 1;
+    int maxIndex = index;
+    int leftIndex = 2 * index + 1;
+    int rightIndex = 2 * index + 2;
+
+    if (leftIndex <= size && data[leftIndex] < data[maxIndex])
+    {
+      maxIndex = leftIndex;
+    }
+    else if(leftIndex <= size && data[leftIndex] == data[maxIndex] && track[leftIndex] < track[maxIndex])
+    {
+      maxIndex = leftIndex;
+    }
+    if (rightIndex <= size && data[rightIndex] < data[maxIndex])
+    {
+      maxIndex = rightIndex;
+    }
+    else if(rightIndex <= size && data[rightIndex] == data[maxIndex] && track[rightIndex] < track[maxIndex])
+    {
+      maxIndex = rightIndex;
+    }
+
+    if (index != maxIndex)
+    {
+      swap(data[index], data[maxIndex]);
+      swap(track[index], track[maxIndex]);
+      siftDown(data, maxIndex, track);
+    }
+  }
+
+public:
+  void Solve()
+  {
     ReadData();
     AssignJobs();
     WriteResponse();
   }
 };
 
-int main() {
+int main()
+{
   std::ios_base::sync_with_stdio(false);
   JobQueue job_queue;
   job_queue.Solve();
