aabf86344c18975ca27aae679be61935b9f718d8
csk1116 <u1116eric777@gmail.com>
2022-12-19 18:13:10 +0800

    finish 6-1.

--- Assignments/starters_edx/week6_search_trees_starters/tree_orders/tree-orders.cpp
+++ Assignments/starters_edx/week6_search_trees_starters/tree_orders/tree-orders.cpp
@@ -5,57 +5,117 @@
 #include <sys/resource.h>
 #endif
 
-using std::vector;
-using std::ios_base;
 using std::cin;
 using std::cout;
+using std::ios_base;
+using std::vector;
 
-class TreeOrders {
+class TreeOrders
+{
   int n;
-  vector <int> key;
-  vector <int> left;
-  vector <int> right;
+  vector<int> key;
+  vector<int> left;
+  vector<int> right;
 
 public:
-  void read() {
+  void read()
+  {
     cin >> n;
     key.resize(n);
     left.resize(n);
     right.resize(n);
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
       cin >> key[i] >> left[i] >> right[i];
     }
   }
 
-
-  vector <int> in_order() {
+  vector<int> in_order()
+  {
     vector<int> result;
     // Finish the implementation
     // You may need to add a new recursive method to do that
-
+    inorderDFS(0, key, left, right, result);
     return result;
   }
 
-  vector <int> pre_order() {
-    vector<int> result;    
-    // Finish the implementation
-    // You may need to add a new recursive method to do that
-    
-    return result;
+  void inorderDFS(int root, const vector<int> &key, const vector<int> &left, const vector<int> &right, vector<int>& result)
+  {
+    if(left[root] == -1 && right[root] == -1)
+    {
+      result.push_back(key[root]);
+      return;
+    }     
+
+    if(left[root] != -1)
+      inorderDFS(left[root], key, left, right, result);
+
+    result.push_back(key[root]);
+
+    if(right[root] != -1)
+      inorderDFS(right[root], key, left, right, result);    
   }
 
-  vector <int> post_order() {
+  vector<int> pre_order()
+  {
     vector<int> result;
     // Finish the implementation
     // You may need to add a new recursive method to do that
-    
+    preorderDFS(0, key, left, right, result);
     return result;
   }
+
+  void preorderDFS(int root, const vector<int> &key, const vector<int> &left, const vector<int> &right, vector<int>& result)
+  {
+    if(left[root] == -1 && right[root] == -1)
+    {
+      result.push_back(key[root]);
+      return;
+    }     
+
+    result.push_back(key[root]);
+
+    if(left[root] != -1)
+      preorderDFS(left[root], key, left, right, result);
+
+    if(right[root] != -1)
+      preorderDFS(right[root], key, left, right, result);   
+  }
+
+  vector<int> post_order()
+  {
+    vector<int> result;
+    // Finish the implementation
+    // You may need to add a new recursive method to do that
+    postorderDFS(0, key, left, right, result);
+    return result;
+  }
+
+  void postorderDFS(int root, const vector<int> &key, const vector<int> &left, const vector<int> &right, vector<int>& result)
+  {
+    if(left[root] == -1 && right[root] == -1)
+    {      
+      result.push_back(key[root]);
+      return;
+    }         
+
+    if(left[root] != -1)
+      postorderDFS(left[root], key, left, right, result);
+
+    if(right[root] != -1)
+      postorderDFS(right[root], key, left, right, result);  
+
+    result.push_back(key[root]);  
+  }
+  
 };
 
-void print(vector <int> a) {
-  for (size_t i = 0; i < a.size(); i++) {
-    if (i > 0) {
+void print(vector<int> a)
+{
+  for (size_t i = 0; i < a.size(); i++)
+  {
+    if (i > 0)
+    {
       cout << ' ';
     }
     cout << a[i];
@@ -63,7 +123,8 @@ void print(vector <int> a) {
   cout << '\n';
 }
 
-int main_with_large_stack_space() {
+int main_with_large_stack_space()
+{
   ios_base::sync_with_stdio(0);
   TreeOrders t;
   t.read();
@@ -73,29 +134,28 @@ int main_with_large_stack_space() {
   return 0;
 }
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
 #if defined(__unix__) || defined(__APPLE__)
   // Allow larger stack space
-  const rlim_t kStackSize = 16 * 1024 * 1024;   // min stack size = 16 MB
+  const rlim_t kStackSize = 16 * 1024 * 1024; // min stack size = 16 MB
   struct rlimit rl;
   int result;
 
   result = getrlimit(RLIMIT_STACK, &rl);
   if (result == 0)
   {
-      if (rl.rlim_cur < kStackSize)
+    if (rl.rlim_cur < kStackSize)
+    {
+      rl.rlim_cur = kStackSize;
+      result = setrlimit(RLIMIT_STACK, &rl);
+      if (result != 0)
       {
-          rl.rlim_cur = kStackSize;
-          result = setrlimit(RLIMIT_STACK, &rl);
-          if (result != 0)
-          {
-              std::cerr << "setrlimit returned result = " << result << std::endl;
-          }
+        std::cerr << "setrlimit returned result = " << result << std::endl;
       }
+    }
   }
 #endif
 
   return main_with_large_stack_space();
 }
-
