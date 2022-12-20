7df67007bc46741c156dde92880a8d635075d079
csk1116 <u1116eric777@gmail.com>
2022-12-20 16:29:05 +0800

    finish 6-3.

--- Assignments/starters_edx/week6_search_trees_starters/is_bst_hard/is_bst_hard.cpp
+++ Assignments/starters_edx/week6_search_trees_starters/is_bst_hard/is_bst_hard.cpp
@@ -7,7 +7,8 @@ using std::cout;
 using std::endl;
 using std::vector;
 
-struct Node {
+struct Node
+{
   int key;
   int left;
   int right;
@@ -16,26 +17,103 @@ struct Node {
   Node(int key_, int left_, int right_) : key(key_), left(left_), right(right_) {}
 };
 
+static bool isFirstNode = true;
+
+void dfs(const vector<Node> &tree, const Node &root, bool &isValid, int &prev, int parent, bool isLeft)
+{
+  if (root.left == -1 && root.right == -1)
+  {
+    if (!isFirstNode)
+    {
+      if (root.key > prev)
+      {
+        isValid = false;
+        return;
+      }
+      else if (root.key == prev)
+      {
+        if (isLeft && root.key >= parent)
+        {
+          isValid = false;
+          return;
+        }
+      }
+    }
+    else
+    {
+      prev = root.key;
+      isFirstNode = false;
+      return;
+    }
+  }
+
+  if (root.right != -1)
+    dfs(tree, tree[root.right], isValid, prev, parent, isLeft);
+
+  if (!isFirstNode)
+  {
+    if (root.key > prev)
+    {
+      isValid = false;
+      return;
+    }
+    else if (root.key == prev)
+    {
+      if (isLeft && root.key >= parent)
+      {
+        isValid = false;
+        return;
+      }
+    }
+    else
+    {
+      prev = root.key;
+    }
+  }
+  else
+  {
+    isFirstNode = false;
+    prev = root.key;
+  }
+
+  if (root.left != -1)
+  {
+    int leftParent = root.key;
+    bool isLeft = true;
+    dfs(tree, tree[root.left], isValid, prev, leftParent, isLeft);    
+  }
+}
+
 bool IsBinarySearchTree(const vector<Node> &tree)
 {
   // Implement correct algorithm here
+  if (tree.empty())
+    return true;
   bool isValid = true;
+  int prev = -1;
+  dfs(tree, tree[0], isValid, prev, -1, false);
   return isValid;
 }
 
-int main() {
+int main()
+{
   int nodes;
   cin >> nodes;
   vector<Node> tree;
-  for (int i = 0; i < nodes; ++i) {
+  for (int i = 0; i < nodes; ++i)
+  {
     int key, left, right;
     cin >> key >> left >> right;
     tree.push_back(Node(key, left, right));
   }
-  if (IsBinarySearchTree(tree)) {
+  if (IsBinarySearchTree(tree))
+  {
     cout << "CORRECT" << endl;
-  } else {
+  }
+  else
+  {
     cout << "INCORRECT" << endl;
   }
   return 0;
 }
+
